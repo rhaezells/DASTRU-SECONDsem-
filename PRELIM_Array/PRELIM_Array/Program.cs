49b449506b70d973d82c906278b6443d53179639
ACT-STUDENT <ACT-STUDENT@LAB2-PC44>
2025-12-16 16:36:31 +0800

    prelims

---
+++ PRELIM_Array/PRELIM_Array/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRELIM_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] countries = new string[5];
+            {
+                //User Input
+                Console.WriteLine("Enter country name:");
+                for (int idx = 0; idx < countries.Length; idx++)
+                {
+                    countries[idx] = Console.ReadLine();
+                }
+                Console.WriteLine("\nThe countries you entered are:");
+
+
+                foreach (string country in countries)
+                {
+                    Console.WriteLine(country);
+                }
+                Array.Sort(countries);
+
+                //Displaying sorted countries
+                Console.WriteLine("\nThe countries in alphabetical order are:");
+                    foreach (string country in countries)
+                {
+                    Console.WriteLine(country);
+                }
+
+                {
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
+
