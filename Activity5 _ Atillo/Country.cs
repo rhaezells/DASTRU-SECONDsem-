42e8a1d15e7130fe5e87f25be6eef0f444500304
John Rhaezel C. Atillo <johnrhaezelatillo@gmail.com>
2026-01-20 17:24:10 +0800

    Add files via upload

---
+++ Activity5 _ Atillo/Country.cs
@@ -0,0 +1,135 @@
+﻿using Activity5___Atillo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity5___Atillo
+{
+    internal class Country
+    {
+        
+        
+       
+         public string Name { get; set; }
+        public string Capital { get; set; }
+        public string Language { get; set; }
+
+        public Country(string name, string capital, string language)
+        {
+            Name = name;
+            Capital = capital;
+            Language = language;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Country  : {Name}");
+            Console.WriteLine($"Capital  : {Capital}");
+            Console.WriteLine($"Language : {Language}");
+        }
+    }
+    class Japan : Country
+    {
+        public string PrimeMinister { get; set; }
+
+        public Japan(string name, string capital, string language, string primeMinister)
+            : base(name, capital, language)
+        {
+            PrimeMinister = primeMinister;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("\n--- Japan Info ---");
+            Console.WriteLine($"Prime Minister: {PrimeMinister}");
+            base.DisplayInfo();
+        }
+    }
+
+    class Philippines : Country
+    {
+        public string President { get; set; }
+
+        public Philippines(string name, string capital, string language, string president)
+            : base(name, capital, language)
+        {
+            President = president;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("\n--- Philippines Info ---");
+            Console.WriteLine($"President: {President}");
+            base.DisplayInfo();
+        }
+    }
+    internal class UserInput
+    {
+        public static string GetString(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+    }
+}
+
+//    internal class Country
+//    {
+//        public string Name { get; set; }
+//        public string Capital { get; set; }
+//        public string Language { get; set; }
+
+
+//        public Country(string name, string capital, string language)
+//        {
+//            Name = name;
+//            Capital = capital;
+//            Language = language;
+//        }
+//        public virtual void displayInfo()     
+//        {
+//            Console.WriteLine($"Country: {Name}");
+//            Console.WriteLine($"Capital : {Capital}");
+//            Console.WriteLine($"Language: {Language}");
+
+//        }
+
+//    }
+//}
+
+//class Japan : Country
+//{
+//    public string PrimeMinester { get; set; }
+//    //Parameterized Constructor
+//    public Japan (string name, string capital, string lanuage, string primeMinester) : base (name, capital, lanuage)
+//    {
+//        PrimeMinester = primeMinester;
+//    }
+
+//    public override void displayInfo()
+//    {
+//        Console.WriteLine("\n---Japan Infor---\n");
+//        Console.WriteLine($"Prime Minester: {PrimeMinester}");
+//        base.displayInfo();
+//    }
+//}
+//// Derived Class 2
+//class Phillipines : Country
+//{
+//    public string President { get; set; }   
+
+//    public Phillipines(string name, string capital, string language, string president) : base (name, capital, language)
+//    {
+//        President = president;
+//    }
+
+//    public override void displayInfo()
+//    {
+//        Console.WriteLine("\n---Phillipines Info---");
+//        Console.WriteLine($"President: {President}");
+//        base.displayInfo ();
+//    }
+//}
---
+++ Activity5 _ Atillo/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity5___Atillo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+       //Name: John Rhaezel C.Atilo         
+            {
+                Country[,] countries = new Country[2, 2];
+
+                Console.WriteLine("=== Enter Japan Details ===");
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.WriteLine($"\nJapan Entry {i + 1}");
+                    string capital = UserInput.GetString("Enter Capital: ");
+                    string language = UserInput.GetString("Enter Language: ");
+                    string primeMinister = UserInput.GetString("Enter Prime Minister: ");
+
+                    countries[0, i] = new Japan("Japan", capital, language, primeMinister);
+                }
+
+                Console.WriteLine("\n=== Enter Philippines Details ===");
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.WriteLine($"\nPhilippines Entry {i + 1}");
+                    string capital = UserInput.GetString("Enter Capital: ");
+                    string language = UserInput.GetString("Enter Language: ");
+                    string president = UserInput.GetString("Enter President: ");
+
+                    countries[1, i] = new Philippines("Philippines", capital, language, president);
+                }
+
+                Console.WriteLine("\n=== Country Information ===");
+                foreach (Country country in countries)
+                {
+                    country.DisplayInfo();
+                }
+
+                Console.ReadKey();
+            }
+        }
+
+
+        //Country[,] countries = new Country[2, 2];
+
+        ////Assign values for each object
+        //countries[0, 0] = new Japan("Japan", "Tokyo", "Japanese", "Fumio Kishida");
+        //countries[0, 1] = new Japan("Japan", "Tokushima","Japanesh/English","Ms. TAKAISHI Sanae");
+
+
+        //countries[1, 0] = new Phillipines("Phillipines", "Cebu", "Cebuano","Pamela Baricuatro");
+        //countries[1, 1] = new Phillipines("Phillipines", "Manila","Tagalog","Ferdinand Marcos Jr.");
+
+        //// Display music information
+        //foreach (Country country in countries)
+        //{
+        //    country.displayInfo();
+        //}
+
+        //Console.ReadKey();
+    }
+}
+
+
